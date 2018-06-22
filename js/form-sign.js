29578767ab8108dde97b468070009300b2cfcebb
Clau1889 <clau.blanco1889@gmail.com>
2018-06-22 09:47:53 -0500

    En este commit se valida no poder agreagar mas de 10 digitos, se da formato y estilo

--- js/form-sign.js
+++ js/form-sign.js
@@ -5,7 +5,7 @@ $(document).ready(function () {
     $('#phone').keydown(getPhoneNumber);
 
     function getPhoneNumber(e) {
-        var charactersNumbers = 10;
+        var charactersNumbers = 9;
         var getCharacters = $(this).val().length;
         var key = e.keyCode;
 
@@ -17,22 +17,16 @@ $(document).ready(function () {
 
         if (getCharacters === charactersNumbers) {
             $('#btn-next').removeClass('disabled');
-        } else {
-            $('#btn-next').addClass('disabled');
+        } else if (getCharacters > charactersNumbers){
+            $("#phone").attr('disabled', 'disabled');
         }
 
     }
 
     function getAsciiChars(key) {
-        return (
-            key == 8  ||           
+        return (          
             key == 9 ||
             key == 13 ||
-            // key == 46 ||
-            // key == 110 ||
-            // key == 190 ||
-            // (key >= 33 && key <= 47) ||
             (key >= 48 && key <= 57));
-        // (key >= 96 && key <= 105));
     }
 });
