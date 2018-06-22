64d614d157d353685580257e937c6a963bb1ec18
Clau1889 <clau.blanco1889@gmail.com>
2018-06-22 01:34:44 -0500

    En este commit se hace funcionalidad para ingresar telefono de la pagina open-sing.html

--- js/form-sign.js
+++ js/form-sign.js
@@ -1,36 +1,38 @@
 $(document).ready(function () {
 
     $('select').formSelect();
- 
-    $('#phone-number').keydown(handlePhoneKeys);
- 
-    function handlePhoneKeys(e){
-        var MIN_CHARS = 9;
-        var inputChars = $(this).val().length;
+
+    $('#phone').keydown(getPhoneNumber);
+
+    function getPhoneNumber(e) {
+        var charactersNumbers = 10;
+        var getCharacters = $(this).val().length;
         var key = e.keyCode;
- 
-        if(!isNumericChar(key)) {
+
+    
+        if (!getAsciiChars(key)) {
             e.preventDefault();
             return;
         }
- 
-        if(inputChars === MIN_CHARS){
-            $('#next-button').removeClass('disabled');
+
+        if (getCharacters === charactersNumbers) {
+            $('#btn-next').removeClass('disabled');
         } else {
-            $('#next-button').addClass('disabled');
+            $('#btn-next').addClass('disabled');
         }
+
     }
- 
-    function isNumericChar(key){
+
+    function getAsciiChars(key) {
         return (
-            // key == 8 ||
-            // key == 9 ||
-            // key == 13 ||
+            key == 8  ||           
+            key == 9 ||
+            key == 13 ||
             // key == 46 ||
             // key == 110 ||
             // key == 190 ||
-            // (key >= 35 && key <= 40) ||
-            (key >= 48 && key <= 57) ||
-            // (key >= 96 && key <= 105));
+            // (key >= 33 && key <= 47) ||
+            (key >= 48 && key <= 57));
+        // (key >= 96 && key <= 105));
     }
- });
+});
